a799fbe127bc6d84f565e6602e08c1be9a1ba97e
westkitestone <westkitestone@gmail.com>
2026-06-30 15:27:57 +0900

    feat: after make fire & extingusher / before add audio components

--- Assets/02_Scripts/Extinguisher/Extinguisher.cs
+++ Assets/02_Scripts/Extinguisher/Extinguisher.cs
@@ -1,44 +1,83 @@
+using FireLink119.Fire;
 using UnityEngine;
 using UnityEngine.XR.Interaction.Toolkit;
 using UnityEngine.XR.Interaction.Toolkit.Interactables;
 
-
-namespace FireLink119.Extinguisher
+public class Extinguisher : MonoBehaviour
 {
-    public class Extinguisher : MonoBehaviour
+    [Header("Particle")]
+    [SerializeField] private ParticleSystem _smokeParticle;
+
+    [Header("Raycast")]
+    [SerializeField] private Transform _rayOrigin;
+    [SerializeField] private float _range = 5f;
+    [SerializeField] private LayerMask _fireLayer;
+
+    private XRGrabInteractable _grabInteractable;
+    private bool isFiring = false;
+
+    private void Awake()
     {
-        [SerializeField] private ParticleSystem smokeParticle;
-        private XRGrabInteractable grabInteractable;
+        _grabInteractable = GetComponent<XRGrabInteractable>();
 
-        void Awake()
-        {
-            grabInteractable = GetComponent<XRGrabInteractable>();
-        }
+        if (_rayOrigin == null)
+            _rayOrigin = transform;
+    }
 
-        void OnEnable()
-        {
-            // 이벤트 구독
-            grabInteractable.activated.AddListener(OnFireStart);
-            grabInteractable.deactivated.AddListener(OnFireEnd);
-        }
+    private void OnEnable()
+    {
+        _grabInteractable.activated.AddListener(OnFireStart);
+        _grabInteractable.deactivated.AddListener(OnFireEnd);
+    }
 
-        void OnDisable()
-        {
-            // 이벤트 해제
-            grabInteractable.activated.RemoveListener(OnFireStart);
-            grabInteractable.deactivated.RemoveListener(OnFireEnd);
-        }
+    private void OnDisable()
+    {
+        _grabInteractable.activated.RemoveListener(OnFireStart);
+        _grabInteractable.deactivated.RemoveListener(OnFireEnd);
+    }
 
-        private void OnFireStart(ActivateEventArgs args)
-        {
-            if (smokeParticle != null) smokeParticle.Play();
-            // TODO: 4번 불 진화 로직 시작 (예: 레이캐스트 또는 트리거 콜라이더 활성화)
-        }
+    private void OnFireStart(ActivateEventArgs args)
+    {
+        if (_smokeParticle != null && !isFiring)
+            _smokeParticle.Play();
 
-        private void OnFireEnd(DeactivateEventArgs args)
+        isFiring = true;
+    }
+
+    private void OnFireEnd(DeactivateEventArgs args)
+    {
+        if (_smokeParticle != null && isFiring)
+            _smokeParticle.Stop();
+
+        isFiring = false;
+    }
+
+    private void Update()
+    {
+        Debug.DrawRay(_rayOrigin.position, _rayOrigin.forward * _range, Color.red);
+
+        if (!isFiring) return;
+
+        if (Physics.Raycast(_rayOrigin.position, _rayOrigin.forward, out RaycastHit hit, _range, _fireLayer, QueryTriggerInteraction.Collide))
         {
-            if (smokeParticle != null) smokeParticle.Stop();
-            // TODO: 4번 불 진화 로직 정지
+            FireObject fire = hit.collider.GetComponentInParent<FireObject>();
+
+            if (fire != null)
+            {
+                fire.TakeExtinguish(Time.deltaTime);
+            }
         }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Transform origin = _rayOrigin != null ? _rayOrigin : transform;
+
+        Gizmos.color = Color.red;
+        Gizmos.DrawLine(origin.position, origin.position + origin.forward * _range);
+
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(origin.position, 0.05f);
+        Gizmos.DrawWireSphere(origin.position + origin.forward * _range, 0.08f);
     }
 }
